625e9c671e51ad8154b6773f61759a644f0f6505
oriolraventosc <oriol.raventos.c@gmail.com>
2023-04-24 01:30:50 +0200

    Setup general styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./components/App/App";
 import "./index.css";
+import mainTheme from "./styles/mainTheme";
+import colors from "./styles/colors";
+import GlobalStyles from "./styles/globalStyles";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { ThemeProvider } from "@mui/material";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +16,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <StyledThemeProvider theme={colors}>
+          <GlobalStyles />
+          <App />
+        </StyledThemeProvider>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
