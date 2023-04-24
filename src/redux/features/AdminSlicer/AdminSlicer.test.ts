674d603d2fc82b148e94a42fdc38e668372f5d93
oriolraventosc <oriol.raventos.c@gmail.com>
2023-04-25 01:55:07 +0200

    Create basic admin functionlaities

--- src/redux/features/AdminSlicer/AdminSlicer.test.ts
+++ src/redux/features/AdminSlicer/AdminSlicer.test.ts
@@ -1,5 +1,8 @@
 import {
   adminLoginActionCreator,
+  acceptOrderActionCreator,
+  cancelOrderActionCreator,
+  loadPendingOrdersActionCreator,
   adminReducer,
   AdminInitialState,
 } from "./AdminSlicer";
@@ -20,6 +23,143 @@ describe("Given a adminReducer", () => {
       const newState = adminReducer(AdminInitialState, action);
 
       expect(newState.isLogged).toBeTruthy();
+    });
+  });
+
+  describe("When it is invoked with the method acceptOrder", () => {
+    test("Then it should delete the product from the pendingOrders list", () => {
+      const action = acceptOrderActionCreator("USB");
+      const initialState = {
+        username: "",
+        password: "",
+        isLogged: false,
+        accessToken: "",
+        id: "",
+        finishedOrders: [],
+        pendingOrders: [
+          {
+            name: "USB SamDisk 2.0 64GB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+          {
+            name: "USB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+        ],
+      };
+      const expectedState = {
+        username: "",
+        password: "",
+        isLogged: false,
+        accessToken: "",
+        id: "",
+        finishedOrders: [],
+        pendingOrders: [
+          {
+            name: "USB SamDisk 2.0 64GB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+        ],
+      };
+
+      const newState = adminReducer(initialState, action);
+
+      expect(newState).toStrictEqual(expectedState);
+    });
+  });
+
+  describe("When it is invoked with the method cancelOrder", () => {
+    test("Then it should delete the product from the pendingOrders list", () => {
+      const action = cancelOrderActionCreator("USB");
+      const initialState = {
+        username: "",
+        password: "",
+        isLogged: false,
+        accessToken: "",
+        id: "",
+        finishedOrders: [],
+        pendingOrders: [
+          {
+            name: "USB SamDisk 2.0 64GB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+          {
+            name: "USB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+        ],
+      };
+      const expectedState = {
+        username: "",
+        password: "",
+        isLogged: false,
+        accessToken: "",
+        id: "",
+        finishedOrders: [],
+        pendingOrders: [
+          {
+            name: "USB SamDisk 2.0 64GB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+        ],
+      };
+
+      const newState = adminReducer(initialState, action);
+
+      expect(newState).toStrictEqual(expectedState);
+    });
+  });
+
+  describe("When it is invoked with the method loadPendingOrders", () => {
+    test("Then it should a list with 2 products", () => {
+      const action = loadPendingOrdersActionCreator([
+        {
+          name: "USB SamDisk 2.0 64GB",
+          price: "10.99€",
+          id: "12345",
+          quantity: 2,
+          image: "usb.jpg",
+        },
+      ]);
+      const expectedState = {
+        username: "",
+        password: "",
+        isLogged: false,
+        accessToken: "",
+        id: "",
+        finishedOrders: [],
+        pendingOrders: [
+          {
+            name: "USB SamDisk 2.0 64GB",
+            price: "10.99€",
+            id: "12345",
+            quantity: 2,
+            image: "usb.jpg",
+          },
+        ],
+      };
+
+      const newState = adminReducer(AdminInitialState, action);
+
+      expect(newState).toStrictEqual(expectedState);
     });
   });
 });
