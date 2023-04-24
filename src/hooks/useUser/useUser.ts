a648fd9cb13168cd1289e0eb633f47f2316272bd
oriolraventosc <oriol.raventos.c@gmail.com>
2023-04-24 02:31:53 +0200

    Create useUser hook and it's getProducts method

---
+++ src/hooks/useUser/useUser.ts
@@ -0,0 +1,20 @@
+import { useAppDispatch } from "../../redux/hooks";
+import { loadProductsActionCreator } from "../../redux/features/userSlicer/userSlicer";
+import { useCallback } from "react";
+import axios from "axios";
+
+const useUser = () => {
+  const apiUrl = process.env.REACT_APP_URL;
+  const dispatch = useAppDispatch();
+  const getProducts = useCallback(async () => {
+    try {
+      const url = `${apiUrl}products/loadAll`;
+      const response = await axios.get(url);
+      const apiResponse = response.data;
+      dispatch(loadProductsActionCreator(apiResponse));
+    } catch {}
+  }, [apiUrl, dispatch]);
+  return { getProducts };
+};
+
+export default useUser;
